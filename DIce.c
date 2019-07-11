d48f95656f1b87361ade10b7243331706e7d3065
s1260161 <52554184+s1260161@users.noreply.github.com>
2019-07-11 21:15:59 +0900

    Add rolling more dice

--- DIce.c
+++ DIce.c
@@ -4,6 +4,7 @@
 int main() {
   int d1 = rand()%6+1;
   int d2 = rand()%6+1;
+  int d3 = rand()%6+1;
   
   char name[20];
   
@@ -15,9 +16,10 @@ int main() {
   printf( "Rolling the dice...\n" );
   printf( "Die 1: %d\n" , d1 );
   printf( "Die 2: %d\n" , d2 );
-  printf( "Total value: %d\n" , d1+d2 );
+  printf( "Die 3: %d\n" , d3 );
+  printf( "Total value: %d\n" , d1+d2+d3 );
   
-  if ( (d1+d2) > 7 ) printf( "%s won!\n" , name );
+  if ( (d1+d2+d3) > 7 ) printf( "%s won!\n" , name );
   else printf( "%s lost!\n" , name );
   
   return 0;
