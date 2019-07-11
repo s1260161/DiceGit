e9734a7525465097f0c76a32728f7ad7f7dbe773
s1260161 <52554184+s1260161@users.noreply.github.com>
2019-07-11 20:27:54 +0900

    Create Dice.c (my first revision)

---
+++ DIce.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+  int d1 = rand()%6+1;
+  int d2 = rand()%6+1;
+  
+  printf( "Rolling the dice...\n" );
+  printf( "Die 1: %d\n" , d1 );
+  printf( "Die 2: %d\n" , d2 );
+  printf( "Total value: %d\n" , d1+d2 );
+  
+  return 0;
+}
